f5ff9ec809207613d0c541aebfcfcd906110c8c7
wkondrac <01158943@pw.edu.pl>
2023-10-30 20:06:58 +0100

    test axios get v1.7

--- src/components/Nfcs/ReadTag/ReadTag.jsx
+++ src/components/Nfcs/ReadTag/ReadTag.jsx
@@ -7,41 +7,40 @@ export default async function readTag() {
     const ndef = new NDEFReader();
     try {
       return new Promise((resolve, reject) => {
-        try {
-          const abortContr = new AbortController();
-          abortContr.signal.onabort = reject;
-          ndef.addEventListener("reading", event => {
-            abortContr.abort();
-            resolve(event);
-          }, { once: true });
-          ndef.scan({ signal: abortContr.signal }).catch(err => reject(err));
-          ndef.onreading = event => {
-            const decoder = new TextDecoder();
-            for (const record of event.message.records) {
-              var today = new Date();
-              var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-              var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds() + ":" + today.getMilliseconds();
-              var dateTime = date + ' ' + time;
-              console.log("1");
-              //var res = axios.get('http://192.168.1.69:3000/api/nfcs/' + decoder.decode(record.data))
-              //var nfc = res.data;
-              const temp = decoder.decode(record.data)
-              logReadTag("---- data ----\n" + decoder.decode(record.data) + "\n" + "TimeStamp: " + dateTime);
-            }
+        const abortContr = new AbortController();
+        abortContr.signal.onabort = reject;
+        ndef.addEventListener("reading", event => {
+          abortContr.abort();
+          resolve(event);
+        }, { once: true });
+        ndef.scan({ signal: abortContr.signal }).catch(err => reject(err));
+        ndef.onreading = event => {
+          const decoder = new TextDecoder();
+          for (const record of event.message.records) {
+            var today = new Date();
+            var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+            var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds() + ":" + today.getMilliseconds();
+            var dateTime = date + ' ' + time;
+            console.log("1");
+            //var res = axios.get('http://192.168.1.69:3000/api/nfcs/' + decoder.decode(record.data))
+            //var nfc = res.data;
+            const temp = decoder.decode(record.data)
+            logReadTag("---- data ----\n" + decoder.decode(record.data) + "\n" + "TimeStamp: " + dateTime);
           }
-        } catch (error) {
-          logReadTag(error);
         }
-
       });
-      //var res = axios.get('http://192.168.1.69:3000/api/nfcs/' + temp)
-     // var nfc = res.data;
+      var res = axios.get('http://192.168.1.69:3000/api/nfcs/' + temp)
+      var nfc = res.data;
     } catch(error) {
-      logReadTag(error);
+      if (error instanceof Error && error.name === 'AbortError') {
+        // work time expired, just return
+      }
+      else{
+        logReadTag(error);
+      }
+      
     }
   } else {
     logReadTag("Web NFC is not supported.");
-    
-    //console.log(nfc);
   }
 }
