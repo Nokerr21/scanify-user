b765958d2a2212ca9c5f5ee5a31feb84944e303b
wkondrac <01158943@pw.edu.pl>
2023-11-15 17:31:14 +0100

    change ui v2.2

--- src/components/Qrs/RenderQrScanner/RenderQrScanner.jsx
+++ src/components/Qrs/RenderQrScanner/RenderQrScanner.jsx
@@ -9,7 +9,7 @@ export function renderQrScanner(){
         supportedScanTypes: [Html5QrcodeScanType.SCAN_TYPE_CAMERA],
         rememberLastUsedCamera: false,
         showTorchButtonIfSupported: true,
-        aspectRatio: 16/9
+        aspectRatio: 1.0
     }
     const html5QrcodeScanner = new Html5QrcodeScanner("readerQR", scannerConfig);
   
