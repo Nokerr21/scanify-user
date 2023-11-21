a801499bdfc066593c17164d6b9560fb89b6d68a
wkondrac <01158943@pw.edu.pl>
2023-11-21 23:04:48 +0100

    change ui v10.5

--- src/components/Nav/NavComponent.jsx
+++ src/components/Nav/NavComponent.jsx
@@ -3,12 +3,17 @@ import './NavComponent.css'
 export default function Nav() {
     return (
         <nav className="nav">
-            <label className="site-title">
-                NFCONTROL
-            </label>
+            <image className='site-logo'></image>
+            <input type="checkbox" id="active" />
+            <label for="active" className="menu-btn"><span></span></label>
+            <label for="active" className="close-btn"></label>
+            <div class="wrapper">
             <ul>
-                <a href="https://nokerr21.github.io/nfcontrol/">About</a>
+                <li><a href="https://nokerr21.github.io/nfcontrol/">Manual</a></li>
+                <li><a href="https://nokerr21.github.io/nfcontrol/">About</a></li>
+                <li><a href='https://nokerr21.github.io/nfcontrol/'>Contact</a></li>
             </ul>
+            </div>
         </nav>
     );
 }
