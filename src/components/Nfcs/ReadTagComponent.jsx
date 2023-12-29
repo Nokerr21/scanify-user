0690eeae72000c78fdbc49352b7d8ea39f1964fb
wkondrac <01158943@pw.edu.pl>
2023-12-29 14:35:21 +0100

    code formatting v1.1

--- src/components/Nfcs/ReadTagComponent.jsx
+++ src/components/Nfcs/ReadTagComponent.jsx
@@ -1,13 +1,12 @@
 import readTag from "./ReadTag/ReadTag";
 import "./ReadTagComponent.css";
-import readTagTemp from "./ReadTag/ReadTagTemp";
 
 
 export function ReadTagComponent() {
     return (
         <div className="row">
             <label className="labl-read">READ NFC</label>
-            <button onClick={() => readTagTemp()} className="btn">Read</button>
+            <button onClick={() => readTag()} className="btn">Read</button>
             <div className="log-read-div">
                 <pre className="log-test" id="logReadTagTest"></pre>
                 <pre className="log" id="logReadTag"></pre>
